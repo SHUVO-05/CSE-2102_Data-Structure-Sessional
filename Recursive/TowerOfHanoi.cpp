a7eee021c082847d2b5b27e84058cc78e15fb638
SHUVO DEBNATH <cfshuvo4@gmail.com>
2024-07-08 00:12:42 +0600

    Tower of Hanoi
    Some change tower of Hanoi

--- Recursive/TowerOfHanoi.cpp
+++ Recursive/TowerOfHanoi.cpp
@@ -1,21 +1,22 @@
 #include<iostream>
 using namespace std;
-void TowerOfHanoi(char start, char End, char Exrra, int n)
+void TowerOfHanoi(char start, char End, char Extra, int n)
 {
-    if(n==1)
-        cout<<"Move disk start "<<start<<" End "<<End<<endl;
+static int coun;
+    if(n==1){coun++;
+   cout<<"\t\t"<<coun<<" Move disk "<<start<<" to End "<<End<<endl;}
     else
     {
-        TowerOfHanoi(start,Exrra,End,n-1);
-        TowerOfHanoi(start,End,Exrra,1);
-        TowerOfHanoi(Exrra,End,start,n-1);
+        TowerOfHanoi(start,Extra,End,n-1);
+        TowerOfHanoi(start,End,Extra,1);
+        TowerOfHanoi(Extra,End,start,n-1);
     }
 }
 int main()
 {
     int n;
     cout<<"\nEnter number of disks: ";
-    cin>>n;
+    cin>>n;temp=n;
     if(n<1)
         cout<<"\n Illegal input..."<<endl;
     else
